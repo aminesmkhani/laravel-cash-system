b6960d3e2cb53a671968b1d05271ceda2941996b
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-10 16:18:27 +0330

    Define Payments Column Table!

--- database/migrations/2021_10_10_122915_create_payments_table.php
+++ database/migrations/2021_10_10_122915_create_payments_table.php
@@ -15,6 +15,13 @@ class CreatePaymentsTable extends Migration
     {
         Schema::create('payments', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('order_id');
+            $table->string('method');
+            $table->string('gateway')->nullable();
+            $table->string('ref_num')->nullable();
+            $table->integer('amount');
+            $table->tinyInteger('status')->comment('0 : Incomplete , 1 : Complete');
+
             $table->timestamps();
         });
     }
