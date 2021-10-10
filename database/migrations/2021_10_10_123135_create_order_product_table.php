35c52940f44b64cd500e69a972c9a00710400c59
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-10 16:22:08 +0330

    Define Pivot Order&Product Column Table!

--- database/migrations/2021_10_10_123135_create_order_product_table.php
+++ database/migrations/2021_10_10_123135_create_order_product_table.php
@@ -15,6 +15,12 @@ class CreateOrderProductTable extends Migration
     {
         Schema::create('order_product', function (Blueprint $table) {
             $table->id();
+            $table->bigInteger('order_id')->unsigned();
+            $table->bigInteger('product_id')->unsigned();
+
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('cascade');
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
+
             $table->timestamps();
         });
     }
