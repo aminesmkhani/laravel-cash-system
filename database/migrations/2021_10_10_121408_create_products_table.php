f825087b8dcf7fa91885dabb656c7d50af7d1eeb
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-10 16:05:54 +0330

    Define Product Column Table!

--- database/migrations/2021_10_10_121408_create_products_table.php
+++ database/migrations/2021_10_10_121408_create_products_table.php
@@ -15,6 +15,10 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('price');
+            $table->string('image');
             $table->timestamps();
         });
     }
