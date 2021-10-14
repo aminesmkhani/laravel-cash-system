d33e234d42e4a07857b5ef557494204f8d05d0dc
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-14 23:12:44 +0330

    Update Cart & Add Image!

--- resources/views/frontend/products/index.blade.php
+++ resources/views/frontend/products/index.blade.php
@@ -3,17 +3,33 @@
 @section('content')
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto">
-            <div class="flex flex-wrap-reverse -m-4">
-                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
-                    <a class="block relative h-48 rounded overflow-hidden">
-                        <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260">
-                    </a>
-                    <div class="mt-4">
-                        <h2 class="text-gray-900 title-font text-lg font-medium">آموزش Tailwindcss</h2>
-                        <button class="mt-2 text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">30,000 هزار تومان</button>
+            <div class=" mx-2 mb-6 sm:mb-0 relative overflow-hidden bg-blue-500 rounded-lg max-w-xs shadow-lg">
+                <svg class="absolute bottom-0 left-0 mb-8" viewBox="0 0 375 283" fill="none">
+                    <rect x="159.52" y="175" width="152" height="152" rx="8" transform="rotate(-45 159.52 175)" fill="#6da3fb">
+                    </rect>
+                    <rect y="107.48" width="152" height="152" rx="8" transform="rotate(-45 0 107.48)" fill="#6da3fb">
+                    </rect>
+                </svg>
+                <div class="relative pt-10 px-10 flex items-center justify-center">
+                    <div class="block absolute w-48 h-48 bottom-0 left-0 -mb-24 ml-3">
+                    </div>
+                    <img class="relative w-40" src="{{asset('aminpanel/src/images/product-1.png')}}" alt="shopping"/>
+                </div>
+                <div class="relative text-white px-6 pb-6 mt-6">
+                    <span class="block opacity-75 -mb-1">
+                        بیرون
+                    </span>
+                    <div class="flex justify-between">
+                        <span class="block font-semibold text-xl">
+                            گلدان
+                        </span>
+                        <span class="bg-white rounded-full text-blue-500  font-bold px-3 py-2 leading-none flex items-center">
+                           ۴۵۰۰۰ تومان
+                        </span>
                     </div>
                 </div>
             </div>
         </div>
     </section>
+
 @endsection
