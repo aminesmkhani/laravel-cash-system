729182a0487c72b55679eed7236192db220771b1
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-14 22:59:36 +0330

    Add Product Cart!

--- resources/views/frontend/products/index.blade.php
+++ resources/views/frontend/products/index.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.front.core')
 
 @section('content')
-    Product Index!
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <div class="flex flex-wrap-reverse -m-4">
+                <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+                    <a class="block relative h-48 rounded overflow-hidden">
+                        <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://dummyimage.com/420x260">
+                    </a>
+                    <div class="mt-4">
+                        <h2 class="text-gray-900 title-font text-lg font-medium">آموزش Tailwindcss</h2>
+                        <button class="mt-2 text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">30,000 هزار تومان</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 @endsection
