24f9a1371191d39c9145093adfbefc7ad1fea5d2
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-15 11:41:54 +0330

    Fix New Code For Seeder :) !

--- database/seeders/ProductTableSeeder.php
+++ database/seeders/ProductTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Product;
 use Illuminate\Database\Seeder;
 
-class ProductSeeder extends Seeder
+class ProductTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
