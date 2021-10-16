9ffdee30956b3d6d0df043a62fb6acb607d46228
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-16 19:07:01 +0330

    Add Implement Methods in SessionStorage!

--- app/Support/Storage/SessionStorage.php
+++ app/Support/Storage/SessionStorage.php
@@ -2,8 +2,45 @@
 
 namespace App\Support\Storage;
 
-class SessionStorage
+use App\Support\Storage\Contracts\StorageInterface;
+use Countable;
+
+class SessionStorage implements StorageInterface , Countable
 {
 
+    public function get($index)
+    {
+        // TODO: Implement get() method.
+    }
+
+    public function set($index, $value)
+    {
+        // TODO: Implement set() method.
+    }
+
+    public function all()
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function exists($index)
+    {
+        // TODO: Implement exists() method.
+    }
+
+    public function unset($index)
+    {
+        // TODO: Implement unset() method.
+    }
+
+    public function clear()
+    {
+        // TODO: Implement clear() method.
+    }
+
+    public function count()
+    {
+        // TODO: Implement count() method.
+    }
 
 }
