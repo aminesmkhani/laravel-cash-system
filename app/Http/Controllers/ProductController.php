0d2a53cef8e5bfa547cc35ad0b1a3b8686092711
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-16 19:48:17 +0330

    Clear!

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,12 +10,6 @@ class ProductController extends Controller
 {
     public function index()
     {
-        $sessionStorage = resolve(StorageInterface::class);
-        $sessionStorage->set('product',5);
-        $sessionStorage->set('item',5);
-        $sessionStorage->set('size',2);
-        ($sessionStorage->unset('size'));
-        dd($sessionStorage->all());
         $products = Product::all();
         return view('frontend.products.index',compact('products'));
     }
