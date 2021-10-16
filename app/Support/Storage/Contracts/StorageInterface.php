50ead4f8245d167141119cbf6b0ade11f3effa2d
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-16 18:59:55 +0330

    define Contracts For all Storage, from now on use this rules!

--- app/Support/Storage/Contracts/StorageInterface.php
+++ app/Support/Storage/Contracts/StorageInterface.php
@@ -5,5 +5,10 @@ namespace App\Support\Storage\Contracts;
 interface StorageInterface
 {
 
-
+    public function get($index);
+    public function set($index, $value);
+    public function all();
+    public function exists($index);
+    public function unset($index);
+    public function clear();
 }
