04b64b9a55a22492a398ad2e807699b787b237ae
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-16 19:15:30 +0330

    Bind SessionInterface in AppServiceprovider!

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Support\Storage\SessionStorage;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(StoreageInterface::class ,function ($app){
+                return new SessionStorage('cart');
+        });
     }
 }
