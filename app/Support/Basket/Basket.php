208dcd71deed6269ce446ef3f410f128b8a16058
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-17 20:01:46 +0330

    set __construct basket & define add function!

--- app/Support/Basket/Basket.php
+++ app/Support/Basket/Basket.php
@@ -2,7 +2,24 @@
 
 namespace App\Support\Basket;
 
+use App\Models\Product;
+use App\Support\Storage\Contracts\StorageInterface;
+
 class Basket
 {
+    private $storage;
 
+
+    public function __construct(StorageInterface $storage)
+    {
+        $this->storage = $storage;
+    }
+
+
+    public function add(Product $product, int $quantity)
+    {
+        $this->storage->set($product->id, [
+           'quantity' => $quantity
+        ]);
+    }
 }
