446c3815c46a7a27201d156e8690aaeb1ac88125
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-10-24 09:26:36 +0330

    complete index function basket controller!:

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -30,7 +30,7 @@ class BasketController extends Controller
 
     public function index()
     {
-        dd($this->basket->all());
-        return view('frontend.basket.basket');
+        $items = $this->basket->all();
+        return view('frontend.basket.basket',compact('items'));
     }
 }
