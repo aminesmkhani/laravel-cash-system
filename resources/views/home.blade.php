7e968da9126c9392f45477f82e8d6f91d6c777e2
Amin Esmkhani <amin.esmkhani75@gmail.com>
2021-11-19 19:15:59 +0330

    Update Home!

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,6 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
                     </div>
                 </div>
             </div>
