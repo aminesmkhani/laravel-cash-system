932552011f19f22815a893469daed3dd4a3543e9
Amin Esmkhani <amin.esmkhani75@gmail.com>
2022-02-02 14:08:35 +0330

    define coupon code rules!

--- app/Support/Cost/Contracts/CostInterface.php
+++ app/Support/Cost/Contracts/CostInterface.php
@@ -4,5 +4,8 @@ namespace App\Support\Cost\Contracts;
 
 interface CostInterface
 {
-
+    public function getCost();
+    public function getTotalCosts();
+    public function persianDescription();
+    public function getSummary();
 }
